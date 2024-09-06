cf00c4ed074d6415b8665be148ff16d69ebca886
MRV <mrv@MRVs-MacBook-Pro.local>
2024-09-06 23:56:48 +0800

    fix: images fast load

--- src/components/Layout/HeaderMenu/index.tsx
+++ src/components/Layout/HeaderMenu/index.tsx
@@ -1,18 +1,13 @@
-'use client'
-import { useMemo, useState, useEffect } from 'react';
+'use client';
+import { useMemo, useState } from 'react';
 import { useRouter, usePathname } from 'next/navigation';
 import Link from 'next/link';
-// import aidBuilderLogoWhite from '../../../assets/images/logo/aid-builder-white.png';
-// import aidBuilderLogo from '../../../assets/images/logo/aid-builder-logo.png';
-import logo from '../../../../public/planned-logo.png'
+import Image from 'next/image'; // Import the Next.js Image component
+import logo from '../../../../public/planned-logo.png';
 import { useScrollPosition } from '../../../hooks/useScrollPosition';
 import type { NextPage } from 'next';
-import styles from './navbar.module.css'
+import styles from './navbar.module.css';
 import { classNames } from '@/app/utility';
-import Button from '../../Button';
-// function classNames(...classes: string[]) {
-//   return classes.filter(Boolean).join(' ');
-// }
 
 type menuItemsType =
     | { id: number; label: string; link: string }
@@ -25,41 +20,11 @@ const menuItems = [
     // { id: 4, label: 'test', link: '/health-professionals#features-section' },
 ];
 
-type BreakpointType = {
-    lg: number;
-    md: number;
-    sm: number;
-    xs: number;
-    xxs: number;
-};
-type ColsType = {
-    lg: number;
-    md: number;
-    sm: number;
-    xs: number;
-    xxs: number;
-};
-const BREAKPOINTS: any = {
-    xxl: 1536,
-    xl: 1280,
-    lg: 1024,
-    md: 768,
-    sm: 640,
-};
-const COLS: any = {
-    xxl: 12,
-    xl: 10,
-    lg: 6,
-    md: 4,
-    sm: 2,
-};
 const NavbarComponent: NextPage<{}> = () => {
     const router = useRouter();
-    const pathname = usePathname()
-
-    const scrollPostion = useScrollPosition();
+    const pathname = usePathname();
+    const scrollPosition = useScrollPosition();
     const [isNavOpen, setIsNavOpen] = useState(false); // initiate isNavOpen state with false
-
 
     const activeMenu = useMemo(() => {
         return menuItems.find((menu) => menu.link === pathname);
@@ -76,20 +41,19 @@ const NavbarComponent: NextPage<{}> = () => {
                     <div className='w-full flex items-center justify-between px-4 md:px-8 lg:px-16 py-2.5'>
                         <div className='flex items-center'>
                             <Link href='/'>
-                                <picture>
-                                    <img
-                                        src={logo.src}
-                                        className='lg:mr-16 cursor-pointer'
-                                        alt='planned-logo'
-                                    />
-                                </picture>
+                                <Image
+                                    src={logo}
+                                    alt='Planned Logo'
+                                    layout='intrinsic' // Adjust layout as needed (e.g., 'fixed', 'responsive')
+                                    className='lg:mr-16 cursor-pointer'
+                                    priority // Optional: Use `priority` for images that are critical to the UI
+                                />
                             </Link>
                         </div>
                     </div>
                 </nav>
             </div>
         </>
-
     );
 };
 
