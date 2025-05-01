8c1164ba16e85e3dcfdc13e66cc9c9b6d223e49e
MRVictoria <mrv@MRVictorias-MacBook-Pro.local>
2025-05-01 10:39:57 +0800

    fix: hero banner and how it works

--- src/components/Layout/HowItWorks/index.tsx
+++ src/components/Layout/HowItWorks/index.tsx
@@ -20,17 +20,18 @@ const HowItWorks: NextPage<{}> = () => {
       return () => window.removeEventListener('resize', updateMobileCheck);
     }, []);
   
+    const selectedImage = isMobile ? howItWorksMobile.src : howItWorks.src;
 
     return (
         <section className="px-3 md:px-8 lg:px-16 py-8 bg-white">
             <h2 className="text-3xl font-bold text-[#1E4D38] mb-[3rem] md:mb-[6.25rem] text-center">how it works</h2>
             <div className="relative overflow-auto flex justify-center">
-                <Image
-                    src={isMobile ? howItWorksMobile.src : howItWorks.src}
+            <img
+                    src={selectedImage}
                     alt="How It Works"
-                    width={800} // Adjusted width for optimal display
-                    height={1600} // Adjusted height for optimal display
-                    className="w-full max-w-[600px] h-auto md:max-w-[800px] lg:max-w-[1000px]" // Responsive width limits
+                    // width={800} // Adjusted width for optimal display
+                    // height={1600} // Adjusted height for optimal display
+                className="w-full max-w-[600px] md:max-w-[800px] lg:max-w-[1000px] h-auto"
                 />
             </div>
         </section>
