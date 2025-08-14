154c0bbf4b1510e10ecd624d93eb528fca671afb
MRVictoria <mrv@MRVictorias-MacBook-Pro.local>
2025-08-14 21:41:13 +0800

    chore: stripe link update, update what’s included and Get started today! section

--- src/components/Layout/GetStarted/index.tsx
+++ src/components/Layout/GetStarted/index.tsx
@@ -13,7 +13,7 @@ const GetStarted: NextPage<{}> = () => {
     const joinNowUrl = selectedPlan === 'monthly'
         // ? 'https://buy.stripe.com/3cs00D3Pyfbv8ak4gj' // 12 USD 
         ? 'https://buy.stripe.com/bJe7sE2j2dvp5Ipext8N205' // 14 USD
-        : 'https://buy.stripe.com/fZe00D3PybZj2Q03cg';
+        : 'https://buy.stripe.com/14AbIU3n62QL9YF7518N206';
 
     return (
         <section className="px-4 md:px-8 lg:px-16 py-8 bg-[#F4F6F0] flex flex-col items-center">
@@ -28,15 +28,15 @@ const GetStarted: NextPage<{}> = () => {
                     >
                         <div className="absolute top-[-1.125rem] right-[-0.1875rem] font-bold text-[#1E4D38] flex flex-row-reverse items-center">
                             <Image src={arrowIcon} alt="Arrow pointing to discount" width={30} height={14} className="ml-2" />
-                            <span className="md:mr-[-2.5rem] mr-[-1.2rem] ml-[-4rem] absolute top-[-25px] text-xl md:text-2xl">59% off!</span>
+                            <span className="md:mr-[-2.5rem] mr-[-1.2rem] ml-[-4rem] absolute top-[-25px] text-xl md:text-2xl">Free Trial</span>
                         </div>
                         {selectedPlan === 'yearly' && (
                             <Image src={checkIcon} alt="Selected check icon" width={20} height={20} className="absolute top-2 left-2" />
                         )}
                         <h3 className="font-bold text-[#1E4D38] mb-2 md:text-[1.0625rem] text-[0.625rem]">YEARLY MEMBERSHIP</h3>
-                        <p className="text-2xl md:text-4xl font-bold text-[#1E4D38]">$5.75</p>
+                        <p className="text-2xl md:text-4xl font-bold text-[#1E4D38]">$4.91</p>
                         <p className={`mb-4 font-bold text-[11px] md:text-base ${selectedPlan === 'yearly' ? 'text-[#1E4D38]' : 'text-[#E1E6C8]'}`}>per month</p>
-                        <p className="text-[0.5rem] md:text-sm  text-[#1E4D38]">$69 billed annually (saves $99 per year!), cancel anytime</p>
+                        <p className="text-[0.5rem] md:text-sm  text-[#1E4D38]">Save 65% - billed yearly at $59</p>
                     </div>
 
                     {/* Monthly Membership - Displayed second on mobile */}
@@ -50,7 +50,7 @@ const GetStarted: NextPage<{}> = () => {
                         <h3 className="font-bold text-[#1E4D38] mb-2 md:text-[1.0625rem] text-[0.625rem]">MONTHLY MEMBERSHIP</h3>
                         <p className="text-2xl md:text-4xl font-bold text-[#1E4D38]">$14</p>
                         <p className={`mb-4 font-bold text-[0.6875rem] md:text-base ${selectedPlan === 'monthly' ? 'text-[#1E4D38]' : 'text-[#E1E6C8]'}`}>per month</p>
-                        <p className="text-[0.5rem] md:text-sm  text-[#1E4D38]">Billed monthly at $14, cancel anytime.</p>
+                        <p className="text-[0.5rem] md:text-sm  text-[#1E4D38]">Billed monthly at $14.</p>
                     </div>
                 </div>
 
@@ -60,9 +60,9 @@ const GetStarted: NextPage<{}> = () => {
                 <div className="flex justify-center mb-2">
                     <Button
                         href={joinNowUrl}
-                        className='!bg-[#1E4D38] !text-[#fff] !rounded-3xl !px-16 !py-6 !font-poppins !text-lg !font-bold mt-[2.1875rem]'>Try For Free</Button>
+                        className='!bg-[#1E4D38] !text-[#fff] !rounded-3xl !px-16 !py-6 !font-poppins !text-lg !font-bold mt-[2.1875rem]'>{selectedPlan === 'monthly' ? 'Join Now' : 'Try For Free'}</Button>
                 </div>
-                <p className="text-[0.75rem] md:text-[1rem] italic font-normal text-center mt-4">7-day free trial. Easily cancel anytime. No commitments</p>
+                <p className="text-[0.75rem] md:text-[1rem] italic font-normal text-center mt-4">Easily cancel anytime. No commitments</p>
             </div>
         </section>
     );
